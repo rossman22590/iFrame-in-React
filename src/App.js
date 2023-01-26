63290bae75c8b1eb931c71fb81353ab4abfea8f6
Ross Cohen <rosscohen1990@gmail.com>
2023-01-26 02:49:11 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="App">
+      <h3>GPT Module</h3>
+      <iframe
+        className="center-iframe"
+        src="https://ai-saas.mytsi.org/login/"
+        style={{ width: "100%", height: "800px" }}
+        scrolling="no"
+      >
+        {" "}
+      </iframe>
+    </div>
+  );
+}
+
+export default App;
